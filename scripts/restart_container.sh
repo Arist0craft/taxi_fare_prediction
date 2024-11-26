c2dd86d5ec6c7e717165b3a962b575a907030183
Arist0craft <dimnktn@gmail.com>
2024-11-26 22:48:10 +0300

    :fix: Removed --rm option in restart_container.sh

--- scripts/restart_container.sh
+++ scripts/restart_container.sh
@@ -1,9 +1,9 @@
 sudo docker stop taxi_fare_prediction || true
+sudo docker rm taxi_fare_prediction || true
 sudo docker pull arist0craft/taxi_fare_prediction:latest
 sudo docker run \
     -d \
     -p 8080:$PORT \
-    --rm \
     --name taxi_fare_prediction \
     --restart always \
     arist0craft/taxi_fare_prediction:latest
