84a657961658d8e09e9f644524f21ca9789d187f
Arist0craft <dimnktn@gmail.com>
2024-11-26 22:25:00 +0300

    :fix: added sudo to restart_container.sh

--- scripts/restart_container.sh
+++ scripts/restart_container.sh
@@ -1,6 +1,6 @@
-docker stop taxi_fare_prediction || true
-docker pull arist0craft/taxi_fare_prediction:latest
-docker run \
+sudo docker stop taxi_fare_prediction || true
+sudo docker pull arist0craft/taxi_fare_prediction:latest
+sudo docker run \
     -d \
     -p 8080:$PORT \
     --rm \
